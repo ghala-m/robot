47fa9d33391e915f6f3ef6e4ca3ef9620009f803
غلا <141947112+ghala-m@users.noreply.github.com>
2023-11-21 00:32:35 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,9 @@
+basic.show_icon(IconNames.HEART)
+basic.show_icon(IconNames.SMALL_HEART)
+basic.show_icon(IconNames.YES)
+basic.show_icon(IconNames.NO)
+basic.show_icon(IconNames.HAPPY)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.SmallHeart)
+basic.showIcon(IconNames.Yes)
+basic.showIcon(IconNames.No)
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+	
+})
