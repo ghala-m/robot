9ae52fdf9566424e7dcdb984679404cf7926273f
ghala-m <141947112+ghala-m@users.noreply.github.com>
2024-02-23 20:31:32 +0300

    done

--- main.py
+++
@@ -1,9 +0,0 @@
-basic.show_icon(IconNames.HEART)
-basic.show_icon(IconNames.SMALL_HEART)
-basic.show_icon(IconNames.YES)
-basic.show_icon(IconNames.NO)
-basic.show_icon(IconNames.HAPPY)
-
-def on_forever():
-    pass
-basic.forever(on_forever)
--- main.ts
+++
@@ -1,8 +0,0 @@
-basic.showIcon(IconNames.Heart)
-basic.showIcon(IconNames.SmallHeart)
-basic.showIcon(IconNames.Yes)
-basic.showIcon(IconNames.No)
-basic.showIcon(IconNames.Happy)
-basic.forever(function () {
-	
-})
